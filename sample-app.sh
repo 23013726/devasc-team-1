6daf4298206bbfb41ab8b8d3e17db1ec6eaff4ed
23013726 <23013726@myrp.edu.sg>
2025-01-24 08:42:09 +0000

    Initial commit with website code

---
+++ sample-app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp sample_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" > tempdir/Dockerfile
+
+cd tempdir
+docker build -t sampleapp .
+
+docker run -t
---
+++ team_1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+
+mkdir tempdir
+
+mkdir tempdir/templates
+
+mkdir tempdir/static
+
+cp team_1.py tempdir/.
+
+cp -r templates/* tempdir/templates/.
+
+cp -r static/* tempdir/static/.
+
+echo "FROM python" > tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+
+echo "COPY  team_1.py /home/myapp/" >> tempdir/Dockerfile
+
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/team_1.py" >> tempdir/Dockerfile
+cd tempdir
+
+docker build -t sampleapp .
+
+docker run -t -d -p 8080:8080 --name team1running sampleapp
+docker ps -a
+
