4be60bbd24a4035ca310382843a36ccb7c600240
hayden-jpg <23015546@myrp.edu.sg>
2025-02-06 09:34:33 +0800

    Update team_1.py

--- team_1.py
+++ team_1.py
@@ -6,10 +6,10 @@ sample = Flask(__name__)
 
 @sample.route("/")
 
-def fmain():
+def main():
     return render_template("index.html")
 
-iff __name__ == "__main__":
+if __name__ == "__main__":
     sample.run(host="0.0.0.0", port=8080)
 
 # Simulating a change to test GitHub Actions | Test 111
